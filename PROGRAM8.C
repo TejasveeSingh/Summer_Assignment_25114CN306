7bd8abe9f300fe9f3b5496f63afd152c2f0b0038
TejasveeSingh <tejasveesingh117@gmail.com>
2026-05-29 21:35:21 +0530

    Day 2 assignment

---
+++ PROGRAM8.C
@@ -0,0 +1,24 @@
+//Program to Check Whether a Number is Palindrome
+#include <stdio.h>
+
+int main() {
+    int num, original, digit, reverse = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while(num > 0) {
+        digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = num / 10;
+    }
+
+    if(original == reverse)
+        printf("Palindrome number");
+    else
+        printf("Not a palindrome number");
+
+    return 0;
+}
