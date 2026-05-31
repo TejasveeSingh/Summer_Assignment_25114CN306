f8365f72ed7aac369b5a2c13b768156b56f8a362
TejasveeSingh <singhtejasvee117@gmail.com>
2026-05-31 22:40:31 +0530

    Day 4 assignment

---
+++ PROGRAM15.C
@@ -0,0 +1,33 @@
+//Write a program to Check Armstrong number.
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n, temp, rem, digits = 0;
+    int sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(temp != 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = n;
+
+    while(temp != 0) {
+        rem = temp % 10;
+        sum += pow(rem, digits);
+        temp /= 10;
+    }
+
+    if(sum == n)
+        printf("%d is an Armstrong Number", n);
+    else
+        printf("%d is not an Armstrong Number", n);
+
+    return 0;
+}
