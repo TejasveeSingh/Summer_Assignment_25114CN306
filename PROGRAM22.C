c5680649bf414657cddd2ba3077264f5154e5f36
TejasveeSingh <singhtejasvee117@gmail.com>
2026-06-02 21:57:34 +0530

    Day 6 assignment

---
+++ PROGRAM22.C
@@ -0,0 +1,21 @@
+//Program to Convert Binary to Decimal
+#include <stdio.h>
+
+int main() {
+    long long binary;
+    int decimal = 0, base = 1, rem;
+
+    printf("Enter a binary number: ");
+    scanf("%lld", &binary);
+
+    while(binary > 0) {
+        rem = binary % 10;
+        decimal += rem * base;
+        base *= 2;
+        binary /= 10;
+    }
+
+    printf("Decimal number = %d", decimal);
+
+    return 0;
+}
