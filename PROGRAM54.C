7783568f4a84a0663ea50d241169a181ef8cec83
TejasveeSingh <singhtejasvee117@gmail.com>
2026-06-14 21:15:00 +0530

    Day 14 assignment

---
+++ PROGRAM54.C
@@ -0,0 +1,29 @@
+//Frequency of an Element
+#include <stdio.h>
+
+int main() {
+    int n, i, key, count = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            count++;
+        }
+    }
+
+    printf("Frequency of %d = %d", key, count);
+
+    return 0;
+}
