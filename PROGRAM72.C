8acc22d379c2c02e90544082fe7ddbf103394bac
TejasveeSingh <singhtejasvee117@gmail.com>
2026-06-18 19:43:47 +0530

    Day 18 assignment

---
+++ PROGRAM72.C
@@ -0,0 +1,24 @@
+//Sort Array in Descending Order
+#include <stdio.h>
+
+int main() {
+    int arr[] = {12, 45, 7, 23, 56, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("Array in descending order: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
