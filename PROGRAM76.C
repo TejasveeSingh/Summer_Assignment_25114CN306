82174ace0e2283b2dd7ef3d80f09e3ac31bebb90
TejasveeSingh <singhtejasvee117@gmail.com>
2026-06-19 12:20:53 +0530

    Day 19 assignment

---
+++ PROGRAM76.C
@@ -0,0 +1,25 @@
+//Write a Program to Find Diagonal Sum
+#include <stdio.h>
+
+int main() {
+    int a[10][10];
+    int n, i, j, sum = 0;
+
+    printf("Enter order of square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        sum = sum + a[i][i];
+    }
+
+    printf("Diagonal Sum = %d", sum);
+
+    return 0;
+}
